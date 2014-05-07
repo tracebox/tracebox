215c1065087f129545b6514aee821d59a9a13eb3
Gregory Detal <gregory.detal@uclouvain.be>
2014-05-07 09:47:52 +0200

    Update libcrafter submodule url
    to use the one from pellgre <https://github.com/pellegre/libcrafter>.
    
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-if [ ! -d "m4" ]; then 
-		mkdir m4
-fi
-
 git submodule init || exit 1
 git submodule update || exit 1
 
-AUTOHEADER=true autoreconf --install || exit 1
+AUTOHEADER=true autoreconf --force --install --verbose || exit 1
 
 ./configure $@
