a75504152d909775a096b7d0ea7ac728998e982d
Gregory Detal <gregory.detal@uclouvain.be>
2014-05-07 10:26:42 +0200

    Fix: autoheader issue
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 git submodule init || exit 1
 git submodule update || exit 1
 
-AUTOHEADER=true autoreconf --force --install --verbose || exit 1
+autoreconf --force --install --verbose || exit 1
 
 ./configure $@
