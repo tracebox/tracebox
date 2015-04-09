477c9fc3d9523ec415fd9759309a20b27e735020
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2015-04-09 10:41:25 +0200

    lua_raw: added bytes/data accessors
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- src/tracebox/lua/lua_raw.cpp
+++ src/tracebox/lua/lua_raw.cpp
@@ -1,7 +1,7 @@
 /**
  * Tracebox -- A middlebox detection tool
  *
- *  Copyright 2013-2015 by its authors. 
+ *  Copyright 2013-2015 by its authors.
  *  Some rights reserved. See LICENSE, AUTHORS.
  */
 
@@ -35,14 +35,59 @@ int l_raw_ref::l_Raw(lua_State *l)
 	return 1;
 }
 
+/***
+ * Get/Set the payload data for this layer
+ * @function data
+ * @tparam[opt] string data
+ * @treturn string data
+ * */
+int l_raw_ref::l_data(lua_State *l)
+{
+	RawLayer *o = l_raw_ref::get(l, 1);
+	if (lua_gettop(l) > 1)
+		o->SetPayload(l_data_type<const char*>::get(l, 2));
+	else
+		l_data_type<std::string>(o->GetStringPayload()).push(l);
+	return 1;
+}
+
+/***
+ * Get/Set the payload bytes for this layer
+ * @function bytes
+ * @tparam[opt] table bytes a list of bytes
+ * @treturn table bytes
+ */
+int l_raw_ref::l_bytes(lua_State *l)
+{
+	RawLayer *o = l_raw_ref::get(l, 1);
+	if (lua_gettop(l) == 1) {
+		lua_newtable(l);
+		const byte *bytes = o->GetPayload().GetRawPointer();
+		for (size_t i = 0; i < o->GetPayloadSize(); ++i) {
+			l_data_type<int>(*bytes).push(l);
+			lua_rawseti(l, -2, i + 1);
+			++bytes;
+		}
+	} else {
+		luaL_checktype(l, 2, LUA_TTABLE);
+		std::vector<byte> bytes(128);
+		for (int i = 1;; ++i, lua_pop(l, 1)) {
+			lua_rawgeti(l, 2, i);
+			if (lua_isnil(l, 2)) {
+				lua_pop(l, 1);
+				break;
+			}
+			bytes.push_back(lua_tointeger(l, -1));
+		}
+		o->SetPayload(&bytes[0], bytes.size());
+	}
+	return 1;
+}
+
 void l_raw_ref::register_members(lua_State *l)
 {
 	l_layer_ref<RawLayer>::register_members(l);
 	meta_bind_func(l, "new", l_Raw);
-	/***
-	 * Set the payload data for this layer
-	 * @function data
-	 * @tparam string data
-	 * */
-	meta_bind_func(l, "data", set_payload<RawLayer>);
+	meta_bind_func(l, "data", l_data);
+	meta_bind_func(l, "bytes", l_bytes);
 }
--- src/tracebox/lua/lua_raw.h
+++ src/tracebox/lua/lua_raw.h
@@ -14,6 +14,8 @@ struct l_raw_ref : public l_layer_ref<Crafter::RawLayer> {
 	using l_layer_ref<Crafter::RawLayer>::l_layer_ref;
 
 	static int l_Raw(lua_State *l);
+	static int l_data(lua_State *l);
+	static int l_bytes(lua_State *l);
 	static void register_members(lua_State *l);
 	static void register_globals(lua_State *l);
 };
