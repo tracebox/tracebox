09d689487faac43a3fa2b474bcd857725fc35c35
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2016-03-07 13:50:43 +0100

    CI: Fixed apt-get options location

--- CI/prepare_common.sh
+++ CI/prepare_common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt-get update -qq
-sudo apt-get install -qq automake libtool lua5.1 liblua5.1-dev liblua5.1 liblua5.1-0-dbg libpcap-dev g++ autoconf gdb libnetfilter-queue-dev libjson0-dev libcurl4-gnutls-dev luarocks
+sudo apt-get -qq -y --force-yes update
+sudo apt-get -qq -y --force-yes install automake libtool lua5.1 liblua5.1-dev liblua5.1 liblua5.1-0-dbg libpcap-dev g++ autoconf gdb libnetfilter-queue-dev libjson0-dev libcurl4-gnutls-dev luarocks
 
 sudo luarocks install penlight
 
