230c41128730019b5dfd3a71f9b19be4f1919f15
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2016-03-07 14:07:12 +0100

    Trusty has gcc >= 4.8

--- CI/travis_prepare.sh
+++ CI/travis_prepare.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sh "$(dirname "$0")/prepare_common.sh"
-
-sudo apt-get install gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
 sh bootstrap.sh
 ./configure $@
