d3f7cf03f844e6e295506d8df6a20210706c15f6
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2017-12-07 12:02:55 +0100

    CI: LDoc requires lua5.1
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- CI/prepare_common.sh
+++ CI/prepare_common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get -qq -y --force-yes update
-sudo apt-get -qq -y --force-yes install automake libtool lua5.2 liblua5.2-dev liblua5.2 liblua5.2-0-dbg libpcap-dev g++ autoconf gdb libnetfilter-queue-dev libjson0-dev libcurl4-gnutls-dev luarocks lua-sec
+sudo apt-get -qq -y --force-yes install automake libtool lua5.1 liblua5.1-dev liblua5.1 liblua5.1-0-dbg libpcap-dev g++ autoconf gdb libnetfilter-queue-dev libjson0-dev libcurl4-gnutls-dev luarocks lua-sec
 
 sudo luarocks install penlight
 
