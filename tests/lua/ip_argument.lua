bda38a3e2a896d863344777b0ac0a737df934319
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2018-01-16 09:11:41 +0100

    Fix 35df82e3734f: Add missing test
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

---
+++ tests/lua/ip_argument.lua
@@ -0,0 +1,10 @@
+--
+-- Tracebox -- A middlebox detection tool
+--
+--  Copyright 2013-2018 by its authors.
+--  Some rights reserved. See LICENSE, AUTHORS.
+--
+--
+
+assert(ip{}:id() ~= ip{}:id())
+assert(ip{id=0}:id() == 0)
